cc70ef9e0f1262aac9c48406a4d734de965f2a7b
Bradley Guthrie <guppy96@gmail.com>
2016-11-03 16:21:42 -0500

    created controller in app and added controller to ng View

--- BasicsOfForms/Scripts/app.js
+++ BasicsOfForms/Scripts/app.js
@@ -1,1 +1,16 @@
 ﻿var app = angular.module("BasicsOfForms", []);
+
+
+app.controller("FormController", ["$scope", function ($scope) {
+
+    $scope.formSubmit = function () {
+        alert("Form Submit called!");
+        
+        console.log();
+
+
+       
+
+    };
+
+}]);
