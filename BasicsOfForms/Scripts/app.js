7408e069922b208b98d95594f429d996c18326fd
Bradley Guthrie <guppy96@gmail.com>
2016-11-03 17:20:20 -0500

    ajax call implemented

--- BasicsOfForms/Scripts/app.js
+++ BasicsOfForms/Scripts/app.js
@@ -3,14 +3,25 @@
 
 app.controller("FormController", ["$scope", function ($scope) {
 
-    $scope.formSubmit = function () {
-        alert("Form Submit called!");
-        
-        console.log();
+    $scope.formSubmit = function (event) {
+        //alert("Form Submit called!");
+        //mixing jquery and angular not best practice
+        var form = $("#AngularForm").serialize();
+        console.log("formmdata" + form);
+
+        $.ajax({
+            url: "/api/AngularApi/post",
+            type: "POST",
+            data:form
+        }).done(function () {
+           
+        });
 
 
+
+
+        event.preventDefault();
        
-
     };
 
 }]);
